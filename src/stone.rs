b95550d97fd86508531e89188c977ee86892d03a
dunlapc4 <conor4@pdx.edu>
2020-02-18 12:35:37 -0800

    Added struct file and made some notes

---
+++ src/stone.rs
@@ -0,0 +1,20 @@
+struct Stone{
+    colour: char,//for now it is a char, later we can do somethign with it
+    liberties: u64, 
+    //do we want a IS DEAD field
+    //anything else?
+
+}
+
+
+impl Stone{
+    //do we want any struct vars for how many are dead for scoring?
+    
+   /* fn new() -> Self {
+        //do a thing
+    }*/
+
+    //other heckin funcs 
+}
+
+
