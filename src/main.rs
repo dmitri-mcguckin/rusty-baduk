d49ab1639e7b47ba7e57b532cf210b6ba290635a
dunlapc4 <conor4@pdx.edu>
2020-03-11 19:16:41 -0700

    Set up skeleton of menu and added in another test. Working on input checking for place fn

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,34 @@ use stone::*;
 use board::*;
 
 fn main() {
+    //make game loop and input system 
+	let mut board = Board::new(19);
+    let mut finished = false;
+    board.draw();
+    while !finished {
+        //if <input> enter coords and switch to other colour also clear and redraw the screen
+        //if <input> make finished = false
+        finished = true;
+    }
+
+}
+
+#[test]
+fn out_of_bounds(){
+    let mut board = Board::new(19);
+    assert_eq!(board.place('A', 20, '@'), None);
+}
+
+#[test]
+fn in_bounds(){
+    let mut board = Board::new(19);
+    assert_eq!(board.place('A', 19, '@'), Some(true));
+}
+
+
+
+
+/*
 	let mut board = Board::new(19);
 	board.place('A', 6, '@');
 	board.place('C', 1, 'O');
@@ -18,12 +46,4 @@ fn main() {
     let option2 = board.place('A', 19, 'O');
     println!("Option returned: {:?}", option);
     println!("Option2 returned: {:?}", option2);
-    board.draw();
-
-}
-
-#[test]
-fn out_of_bounds(){
-    let mut board = Board::new(19);
-    assert_eq!(board.place('A', 20, '@'), None);
-}
+    board.draw();*/
